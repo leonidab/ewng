ac9de51b86ba9004c921550037b8fb78bc1417bb
leonidab <leonidabrahams@gmail.com>
2019-03-26 17:11:00 +0100

    Unit test

---
+++ src/test/java/com/leonid/eurowings/controller/UserControllerTest.java
@@ -0,0 +1,33 @@
+package com.leonid.eurowings.controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testThisThing() throws Exception {
+
+        this.mockMvc.perform(get("/allusers")).andExpect(status().isOk());
+        MvcResult result = this.mockMvc.perform(get("/allusers")).andReturn();
+        Assert.assertEquals(result.getResponse().getContentType(), "application/json;charset=UTF-8");
+
+    }
+
+}
